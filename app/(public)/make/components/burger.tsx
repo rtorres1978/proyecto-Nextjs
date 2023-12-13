8583d81217d024bb4bb1f388c0d29c884d1c3479
rtorres1978 <com-sic@hotmail.com>
2023-12-13 09:50:51 -0500

    cambio

--- app/(public)/make/components/burger.tsx
+++ app/(public)/make/components/burger.tsx
@@ -6,55 +6,61 @@ interface Props {
   burger: Ingredient[];
 }
 const MakeBurger = ({ burger }: Props) => {
-  let counter = 0;
+  const[index, setIndex] = useState(0);
 
-  
+  // useEffect(() => {
+  //   if(index === burger.length) return;
+  //   setIndex((prev) => prev + 1);
+  // }, [burger])
+
   return (
     <div
       className="bg-[url('/images/make/bg.png')] h-[550px] w-full
     bg-contain bg-no-repeat bg-center relative flex justify-center"
     >
-   
+    {/* {index > 0 && (
+              <Image
+             
+                src="/images/make/bun-defa.png"
+                width={1000}
+                height={300}
+                alt='Bun Default'
+                className="absolute z-20"
+                style={{
+                  bottom: `${(burger.length) * 40}px`,
+                }}
+              />
+            )} */}
+         
+
       {burger.map(
         ({ alt, id, image, name, price, quantity, rotate }, i) => (
           <>
-           {
-             i === burger.length - 1 && counter > 0 && (
+            {i === burger.length - 1 && (
               <Image
-              key={i}
-              src="/images/make/bun-defa.png"
-              width={1000}
-              height={300}
-              alt='Bun Default'
-              className="absolute z-20"
-              style={{
-                bottom: `${(counter + 1) * 40}px`,
-              }}
-            />
-             )
-           }
-
-          {quantity > 0 &&
-            Array.from({length: quantity}, (_,idx) => {
-            counter++
-            return(
-              <Image
-              key={idx}
-              src={image}
-              alt={alt}
-              width={1000}
-              height={300}
-              className="absolute z-10"
-              style={
-                {
-                  bottom: `${counter * 40}px`
-                }
-              }
+             
+                src="/images/make/bun-defa.png"
+                width={1000}
+                height={300}
+                alt='Bun Default'
+                className="absolute z-20"
+                style={{
+                  bottom: `${(i + 2) * 40}px`,
+                }}
               />
-            )
-            })
+            )}
 
-          }
+           <Image
+                key={id}
+                src={image}
+                width={500}
+                height={500}
+                alt={alt}
+                className="absolute z-10"
+                style={{
+                  bottom: `${(i+1) * 40}px`,
+                }}
+              />
           </>
              
         )
